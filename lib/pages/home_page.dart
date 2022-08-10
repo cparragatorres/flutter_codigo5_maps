116ba8066fa45c3c0de438344f5f4444924e4810
cparragatorres <humbertpt14@gmail.com>
2022-08-09 21:19:37 -0500

    Google Maps 1.7

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,12 @@
 import 'dart:convert';
 
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_codigo5_maps/utils/map_style.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -48,7 +54,57 @@ class _HomePageState extends State<HomePage> {
     },
   ];
 
-  // GeolocatorPlatform _geolocatorPlatform = GeolocatorPlatform.instance;
+  @override
+  initState() {
+    super.initState();
+    getDataMarkers();
+  }
+
+  getDataMarkers() async {
+    listLocation.forEach((element) async {
+      //   Uint8List bytes = await imageToBytes(element["icon"], fromNetwork: true);
+      //   BitmapDescriptor icon = BitmapDescriptor.fromBytes(bytes);
+      //   MarkerId markerId = MarkerId(_markers.length.toString());
+      //   Marker marker = Marker(
+      //     markerId: markerId,
+      //     icon: icon,
+      //     position: LatLng(element["lat"], element["lon"]),
+      //   );
+      //   _markers.add(marker);
+
+      imageToBytes(element["icon"], fromNetwork: true).then((value){
+        MarkerId markerId = MarkerId(_markers.length.toString());
+        Marker marker = Marker(
+          markerId: markerId,
+          icon: BitmapDescriptor.fromBytes(value),
+          position: LatLng(element["lat"], element["lon"]),
+        );
+        _markers.add(marker);
+        setState((){});
+      });
+    });
+  }
+
+  Future<Uint8List> imageToBytes(String path,
+      {int width = 100, bool fromNetwork = false}) async {
+    late Uint8List bytes;
+
+    if (fromNetwork) {
+      File file = await DefaultCacheManager().getSingleFile(path);
+      bytes = await file.readAsBytes();
+    } else {
+      ByteData byteData = await rootBundle.load(path);
+      bytes = byteData.buffer.asUint8List();
+    }
+    final codec = await ui.instantiateImageCodec(bytes, targetWidth: width);
+    ui.FrameInfo frame = await codec.getNextFrame();
+
+    ByteData? myByteFormat = await frame.image.toByteData(
+      format: ui.ImageByteFormat.png,
+    );
+
+    return myByteFormat!.buffer.asUint8List();
+  }
 
   Future<CameraPosition> initCurrentPosition() async {
     Position position = await Geolocator.getCurrentPosition();
@@ -60,8 +116,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    initCurrentPosition();
-
     return Scaffold(
       body: FutureBuilder(
         future: initCurrentPosition(),
@@ -74,16 +128,18 @@ class _HomePageState extends State<HomePage> {
               mapType: MapType.normal,
               compassEnabled: true,
               onMapCreated: (GoogleMapController controller) {
-                controller.setMapStyle(json.encode(mapStyle2));
+                controller.setMapStyle(json.encode(mapStyle));
               },
               markers: _markers,
               onTap: (LatLng position) async {
                 MarkerId markerId = MarkerId(_markers.length.toString());
                 Marker marker = Marker(
                   markerId: markerId,
-                  // icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
-                  icon: await BitmapDescriptor.fromAssetImage(
-                      ImageConfiguration(), 'assets/icons/location-icon.png'),
+                  // icon: await BitmapDescriptor.fromAssetImage(ImageConfiguration(), 'assets/icons/location-icon.png'),
+                  icon: BitmapDescriptor.fromBytes(await imageToBytes(
+                      "assets/icons/place.png",
+                      width: 50,
+                      fromNetwork: false)),
                   position: position,
                   rotation: -4,
                   draggable: true,
